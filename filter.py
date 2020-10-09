9435e7f21bd35deb804591fa97021c175f0d1996
simonsruggi <50123841+simonsruggi@users.noreply.github.com>
2020-10-09 13:58:41 +0200

    Add files via upload

---
+++ filter.py
@@ -0,0 +1,20 @@
+import sys
+path = input('Input path: ')
+
+
+try:
+    
+    with open(path,'r') as f:
+        myfile = f.read().split('\n')
+except:
+    
+    print('Invalid path')
+    sys.exit()
+    
+mynewlist = []
+
+print('New list: \n')
+for i in myfile:
+    if i not in mynewlist:
+        mynewlist.append(i)
+        print(i)
